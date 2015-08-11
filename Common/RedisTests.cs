d4c526cfd1187cdf70cd2bace3fde65735354d44
timbarrass <tim.barrass@gmail.com>
2015-08-11 21:41:17 +0100

    First Redis exploratory test
    Wanted to get some confidence that the redis list length fetch of of
    O(1) and not O(N) complexity, and I don't have the code to hand

---
+++ Common/RedisTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StackExchange.Redis;
+using System;
+
+namespace Redis.Workflow.Common
+{
+    [TestClass]
+    public class RedisTests 
+    {
+        [TestMethod]
+        // To monitor I'll want to query list lengths -- I want to be happy that getting 
+        // list length is O(1) rather than O(N), or I'll have to keep track of the list
+        // size myself.
+        public void ListLengthComplexity()
+        {
+            var mux = ConnectionMultiplexer.Connect("localhost");
+
+            var db = mux.GetDatabase();
+
+            var maxListLength = 100000;
+            var listLength = 1;
+
+            while (listLength < maxListLength)
+            {
+                for (var i = 0; i < listLength; i++)
+                {
+                    db.ListLeftPush("theList", new RedisValue[] { i.ToString() });
+                }
+
+                var iterationsRemaining = 100000;
+
+                DateTime start = DateTime.Now;
+
+                while (iterationsRemaining-- > 0)
+                {
+                    var len = db.ListLength("theList");
+                }
+
+                var duration = new TimeSpan(DateTime.Now.Ticks - start.Ticks);
+
+                var average = duration.Ticks / 100000;
+
+                Console.WriteLine("listLength " + listLength + " duration " + duration + " average " + average);
+
+                listLength *= 10;
+            }
+
+            Assert.IsTrue(false);
+        }
+    }
+}
