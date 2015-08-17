64174d190736b0c9afb27cebc78434cd6ec9d0f0
timbarrass <tim.barrass@gmail.com>
2015-08-17 08:17:12 +0100

    Quick comment correction

--- ExampleApp/ThreadPoolTaskHandler.cs
+++ ExampleApp/ThreadPoolTaskHandler.cs
@@ -4,9 +4,9 @@ using System;
 namespace Redis.Workflow.ExampleApp
 {
     /// <summary>
-    /// A simple example task handler. It spins up a thread on the thread pool that does
-    /// nothing but state it's doing work, and call the workflow result handler to keep
-    /// things ticking over.
+    /// A simple example task handler. It spins up a new task that does nothing but 
+    /// state it's doing work, and call the workflow result handler to keep things 
+    /// ticking over.
     /// </summary>
     public class ThreadPoolTaskHandler : ITaskHandler
     {
