fabe94ed40eb747b557ec384d6d71ebbd24c15a1
timbarrass <tim.barrass@gmail.com>
2015-08-19 19:23:42 +0100

    Commented IResultHandler

--- Common/IResultHandler.cs
+++ Common/IResultHandler.cs
@@ -2,8 +2,16 @@
 {
     public interface IResultHandler
     {
+        /// <summary>
+        /// Should be called on successful task completion. Will cause
+        /// child tasks, if any, to be submitted.
+        /// </summary>
         void OnSuccess();
 
+        /// <summary>
+        /// Should be called on task failure. Will not cause child tasks
+        /// to be submitted, and will cause workflow to end abruptly.
+        /// </summary>
         void OnFailure();
     }
 }
